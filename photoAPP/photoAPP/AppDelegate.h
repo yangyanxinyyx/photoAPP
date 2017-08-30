8dc1662f59a0caae0fa104f44a0b9b97b53d695e
yanyx <yanyx@adnonstop.com>
2017-08-30 11:12:32 +0800

    添加Xcode 工程

---
+++ photoAPP/photoAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  photoAPP
+//
+//  Created by admin on 30/8/17.
+//  Copyright © 2017年 YYX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ photoAPP/photoAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  photoAPP
+//
+//  Created by admin on 30/8/17.
+//  Copyright © 2017年 YYX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
